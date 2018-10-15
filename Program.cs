06940f3c75768bc1bfe3a3a5921c3f314072c728
Ellekwis <43903852+Ellekwis@users.noreply.github.com>
2018-10-15 18:05:08 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Создано в SharpDevelop.
+ * Пользователь: Артём
+ * Дата: 27.09.2018
+ * Время: 19:16
+ * 
+ * Для изменения этого шаблона используйте меню "Инструменты | Параметры | Кодирование | Стандартные заголовки".
+ */
+using System;
+
+namespace lab1nom5
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			
+			// TODO: Implement Functionality Here
+			Console.WriteLine("Введите номер месяца от 1 до 12");
+			int x=int.Parse(Console.ReadLine());
+			if (x>=3&&x<=5)
+				Console.WriteLine("Весна");
+else
+	if (x>=6&&x<=8)
+		Console.WriteLine("Лето");
+else
+	if (x>=9&&x<=11
+	   )Console.WriteLine("Осень");
+else
+if (x==12||x<=2)
+		Console.WriteLine("Зима");
+Console.ReadKey();
+			
+			
+			
+		}
+	}
+}
